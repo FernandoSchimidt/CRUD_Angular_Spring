7e6c8584067bcb6fa1f7b2adb3c267cde74186fa
Fernando Schimidt <40704350+FernandoSchimidt@users.noreply.github.com>
2024-03-05 16:59:07 -0300

    Been Validator cursos

--- crud-spring/src/main/java/com/schimidt/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/schimidt/crudspring/controller/CourseController.java
@@ -2,14 +2,19 @@ package com.schimidt.crudspring.controller;
 
 import com.schimidt.crudspring.model.Course;
 import com.schimidt.crudspring.repository.CourseRepository;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/courses")
 @AllArgsConstructor
@@ -24,20 +29,20 @@ public class CourseController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody Course course){
+    public Course create(@RequestBody @Valid Course course){
         return repository.save(course);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Course> findById(@PathVariable Long id){
+    public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id){
        return repository.findById(id)
                .map(record -> ResponseEntity.ok().body(record))
                .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Course> update( @PathVariable Long id,
-                                          @RequestBody Course course){
+    public ResponseEntity<Course> update( @PathVariable @NotNull @Positive Long id,
+                                          @RequestBody @Valid Course course){
        return  repository.findById(id)
                .map(recordFound -> {
                    recordFound.setName(course.getName());
@@ -49,7 +54,7 @@ public class CourseController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id){
+    public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id){
                return repository.findById(id)
                 .map(recordFound -> {
                     repository.deleteById(id);
--- crud-spring/src/main/java/com/schimidt/crudspring/model/Course.java
+++ crud-spring/src/main/java/com/schimidt/crudspring/model/Course.java
@@ -2,10 +2,14 @@ package com.schimidt.crudspring.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @Entity
@@ -16,8 +20,16 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @JsonProperty("id")
     private Long id;
-    @Column(name = "name",length = 200,nullable = false)
+
+    @Column(name = "name",length = 100,nullable = false)
+    @NotNull
+    @NotBlank
+    @Length(min = 5,max = 100)
     private String name;
+
     @Column(name="category",length = 20,nullable = false)
+    @NotNull
+    @Length(max = 50)
+    @Pattern(regexp = "Back-end|Front-end")
     private String category;
 }
